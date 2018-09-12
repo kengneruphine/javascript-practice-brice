ca2faa418c6e62396ef0a2b4761f28a6f38d82e1
Jumeb <bricejume@gmail.com>
2018-09-12 14:27:35 +0100

    Completed the sequence exercise

--- Chapter-6-Life-of-Objects/sequence.js
+++ Chapter-6-Life-of-Objects/sequence.js
@@ -2,28 +2,31 @@
 
 function sequence(data) {
     var info = data;
-    var i = 0;
-    function iterString() {
-        if(info instanceof Array)
-            info = info.join('');
-        else
-            info = info.toString();
-        var len = info.length
-        if (i <= len){
-            return(info.charAt(i))
-            i+=1;
+    var string = '';
+    if(info instanceof Array) {
+        for (var i = 0; i < info.length; i++){
+            string = string.concat(info[i]);
         }
+        return(string);
+    }
+    else{
+        string = info.toString();
+        return(string)
     }
 }
-
-/*function LogFive(seqObj) {
-    var i = 0
-    if (i <= 5) {
-        console.log(seqObj)
+//console.log(sequence([1,23,3,45]))
+function logFive(seqObj) {
+    var i = 0;
+    var info = seqObj;
+        while (i < 5){
+            if(info.charAt(i))
+                console.log(info.charAt(i));
+            i++;
     }
+    
 }
-console.log(LogFive(sequence([1,2,3,4,5,6,7,8,9,10,11,12,13])))
-
-*/
-//Incomplete
-
+logFive(sequence([1, 2 ,3, 4, 5, 6, 7]));
+console.log('\n');
+logFive(sequence('I am a girl'));
+console.log('\n');
+logFive(sequence(123));
